5ad4cc782034bb2859bbf7ffe7178d86e7212337
moni <muwingky@gmail.com>
2026-06-12 15:20:54 +0900

    Refactor: 메시지 박스 왼쪽 정렬 수정, 장소에 링크 삽입

--- FrontEnd/src/components/PartyInfoBox.tsx
+++ FrontEnd/src/components/PartyInfoBox.tsx
@@ -11,7 +11,11 @@ export default function PartyInfoBox() {
       contentClassName="space-y-1 text-center text-[13px] text-[#4b3b3b]"
     >
       <p>일시: {date}</p>
-      <p>장소: {place}</p>
+      <p>장소:
+        <a href="https://map.naver.com/p/entry/place/32407273?c=15.00,0,0,0,dh" target="_blank" rel="noopener noreferrer" className="underline text-blue-700">
+          {place}
+        </a>
+      </p>
     </Box>
   );
 }
